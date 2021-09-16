7ec1a749b2decef781e86693969ab24ccb2cb0a4
mingsomm <alswlkku@ajou.ac.kr>
2021-09-17 00:56:57 +0900

    springboot 회원가입

---
+++ springboot_signup/fastlms/src/main/java/com/zerobase/fastlms/member/MemberController.java
@@ -0,0 +1,12 @@
+package com.zerobase.fastlms.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    @GetMapping("/member/register")
+    public String register(){
+        return "member/register";
+    }
+}
