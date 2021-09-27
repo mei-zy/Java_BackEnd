274ad430cebd5b102fbc882d180c6863f737f27d
mingsomm <alswlkku@ajou.ac.kr>
2021-09-27 18:54:41 +0900

    week7_admin

---
+++ springboot_admin/src/main/java/com/example/homepage/AdminController.java
@@ -0,0 +1,8 @@
+package com.example.homepage;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+}
